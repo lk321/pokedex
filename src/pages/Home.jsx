39335a3b4e877e4af33a8d5b7579671e2155478c
Alexander Jocobi <alexander.vazquez@g-global.com>
2022-01-24 18:49:49 -0800

    new button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,13 @@ import {
   Container,
   Typography,
 } from '@mui/material';
-
 import PokeList from 'components/PokeList';
+import CustomSwitch from 'components/CustomSwitch';
 
 function HomePage() {
   return (
     <Container>
+      <CustomSwitch />
       <Typography variant="h2">Pokédex with SWR</Typography>
       <PokeList />
     </Container>
