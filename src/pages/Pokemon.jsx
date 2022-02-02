1097d7a878fa1f75f68adcf393a6409c92be44d3
Antonio Orozco <antonio.orozco@g-global.com>
2022-02-01 19:20:25 -0800

    stants features

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -8,15 +8,14 @@ import {
   Typography,
   CircularProgress,
 } from '@mui/material';
+
+import {
+  FlagTwoTone as FlagTwoToneIcon,
+  HeightTwoTone as HeightTwoToneIcon,
+  FitnessCenterTwoTone as FitnessCenterTwoToneIcon,
+} from '@mui/icons-material';
+
 import fetcher from 'utils/fetcher';
-
-import
-{
-  FaTextHeight,
-  FaDownload,
-} from 'react-icons/fa';
-import { BsFillFlagFill } from 'react-icons/bs';
-
 import { TYPES } from 'utils/constants';
 
 function Pokemon() {
@@ -30,6 +29,8 @@ function Pokemon() {
 
   if (!data) return <CircularProgress />;
 
+  const [{ type: principalType }] = data.types;
+
   return (
     <Grid container justifyContent="center" alignItems="center">
       <Stack spacing={2} justifyContent="center" alignItems="center">
@@ -37,10 +38,10 @@ function Pokemon() {
           alt={data.name}
           src={data.sprites.front_default}
           sx={{
-            height: 165,
-            width: 165,
+            height: 180,
+            width: 180,
             borderWidth: 5,
-            borderColor: 'error.main',
+            borderColor: TYPES[principalType.name],
             borderStyle: 'solid',
           }}
         />
@@ -57,11 +58,21 @@ function Pokemon() {
               key={slot}
               label={type.name}
               variant="outlined"
-              sx={{ color: TYPES[type.name], borderColor: TYPES[type.name] }}
+              sx={{
+                color: TYPES[type.name],
+                borderColor: TYPES[type.name],
+              }}
             />
           ))}
         </Stack>
-        <Grid sx={{ p: 6, color: 'text.primary' }}>
+        <Grid
+          sx={{
+            p: 6,
+            color: 'text.primary',
+            borderRadius: 6,
+            backgroundColor: 'background.neutral',
+          }}
+        >
           <Stack
             direction="row"
             justifyContent="center"
@@ -74,9 +85,12 @@ function Pokemon() {
               alignItems="center"
               spacing={0.5}
             >
-              <FaTextHeight />
-              <Typography variant="inherit"> Heigth </Typography>
-              <Typography variant="caption">0.88 </Typography>
+              <HeightTwoToneIcon />
+              <Typography variant="inherit" component="b">Heigth</Typography>
+              <Typography variant="caption">
+                {data.height}
+                m
+              </Typography>
             </Stack>
             <Stack
               direction="column"
@@ -84,9 +98,12 @@ function Pokemon() {
               alignItems="center"
               spacing={0.5}
             >
-              <FaDownload />
-              <Typography variant="inherit"> Weight </Typography>
-              <Typography variant="caption">6.0 K </Typography>
+              <FitnessCenterTwoToneIcon />
+              <Typography variant="inherit" component="b">Weight</Typography>
+              <Typography variant="caption">
+                {data.weight}
+                kg
+              </Typography>
             </Stack>
             <Stack
               direction="column"
@@ -94,9 +111,11 @@ function Pokemon() {
               alignItems="center"
               spacing={0.5}
             >
-              <BsFillFlagFill />
-              <Typography variant="inherit"> Abilities </Typography>
-              <Typography variant="caption">Lighting Rod</Typography>
+              <FlagTwoToneIcon />
+              <Typography variant="inherit" component="b">Abilities</Typography>
+              <Typography variant="caption">
+                {data.abilities.map(({ ability }) => ability.name).join(', ')}
+              </Typography>
             </Stack>
           </Stack>
         </Grid>
--- src/theme/palette.js
+++ src/theme/palette.js
@@ -1,10 +1,14 @@
+import { alpha } from '@mui/material/styles';
+
 export const GREY = {
   0: '#ffffff',
   100: '#f9fafb',
   400: '#e1e7ef',
   500: '#919EAB',
   600: '#8e99ab',
+  700: '#212b36',
   800: '#212b36',
+  500_12: alpha('#919EAB', 0.12),
 };
 
 export default function palette(mode) {
@@ -25,9 +29,14 @@ export default function palette(mode) {
       contrastText: '#000000',
     },
     text: {
-      primary: isLigth ? GREY[800] : '#ffffff',
+      primary: isLigth ? GREY[700] : GREY[400],
       secondary: isLigth ? GREY[600] : GREY[400],
       disabled: isLigth ? GREY[400] : GREY[600],
+    },
+    background: {
+      default: isLigth ? GREY[0] : GREY[800],
+      paper: isLigth ? GREY[600] : GREY[800],
+      neutral: isLigth ? GREY[100] : GREY[500_12],
     },
     grey: GREY,
   };
